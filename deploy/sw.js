295488f988d45b82310d8f9cf3b1ed900aa1b18a
seuth325 <seuth325@github.com>
2026-07-16 16:58:12 -0400

    Harden homepage cache clearing

---
+++ deploy/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(Promise.all([
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
+    self.registration.unregister(),
+    self.clients.claim(),
+  ]));
+});
+self.addEventListener('fetch', () => {});
